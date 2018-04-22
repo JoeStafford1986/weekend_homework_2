edcb2939e80a85e9f8585df16e7e9b617affd2ff
Joe Stafford <joestafford1986@gmail.com>
2018-04-22 20:49:46 +0100

    fixed inheritance problem with class declaration, added getter tests for drink class / tests pass

--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -3,12 +3,12 @@ require('minitest/rg')
 require_relative('../bar.rb')
 require_relative('../drink.rb')
 
-class BarTest
+class BarTest < MiniTest::Test
 
   def setup
     @drink = Drink.new("beer", 1)
     @drink_1 = Drink.new("wine", 2)
-    
+
     @bar = Bar.new()
   end
 
--- specs/drink_spec.rb
+++ specs/drink_spec.rb
@@ -2,10 +2,18 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../drink.rb')
 
-class DrinkTest
+class DrinkTest < MiniTest::Test
 
   def setup
     @drink = Drink.new("beer", 1)
+  end
+
+  def test_get_name
+    assert_equal("beer", @drink.name())
+  end
+
+  def test_get_price
+    assert_equal(1, @drink.price())
   end
 
 end
