31919f554b6c01b9f434b755d0009b47124fedca
Joe Stafford <joestafford1986@gmail.com>
2018-04-22 21:43:36 +0100

    added check_stock method to bar / tests pass

--- bar.rb
+++ bar.rb
@@ -12,8 +12,15 @@ class Bar
     @stock << drink
   end
 
-  def check_stock(drink_name)
-
+  def check_stock(drink_name_to_find)
+    return if (stock_count() == 0)
+    @stock.each do |drink|
+      if drink.name() == drink_name_to_find
+        return true
+      else
+        return false
+      end
+    end
   end
 
 end
