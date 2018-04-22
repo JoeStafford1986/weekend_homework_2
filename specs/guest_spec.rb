8e9e0a4b235d1165e9f3f32c842fa513f57b7cea
Joe Stafford <joestafford1986@gmail.com>
2018-04-22 09:22:55 +0100

    added method to check name of guest, guest class initialize added / tests passing

--- specs/guest_spec.rb
+++ specs/guest_spec.rb
@@ -4,4 +4,16 @@ require_relative('../guest.rb')
 
 class GuestTest < MiniTest::Test
 
+  def setup
+    @guest = Guest.new("Joe Stafford")
+  end
+
+  def test_can_create_song()
+    assert_equal(Guest, @guest.class())
+  end
+
+  def test_get_name
+    assert_equal("Joe Stafford", @guest.name())
+  end
+
 end
