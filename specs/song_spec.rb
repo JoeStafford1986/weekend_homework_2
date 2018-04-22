b3c9df9d64742a1b3572e8211d97f25ae54ad68e
Joe Stafford <joestafford1986@gmail.com>
2018-04-22 09:17:25 +0100

    added get_name method for song class / test pass

--- specs/song_spec.rb
+++ specs/song_spec.rb
@@ -4,4 +4,12 @@ require_relative('../song.rb')
 
 class SongTest < MiniTest::Test
 
+  def setup
+    @song = Song.new("Bohemian Rhapsody")
+  end
+
+  def test_get_name
+    assert_equal("Bohemian Rhapsody", @song.name())
+  end
+
 end
